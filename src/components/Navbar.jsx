986bb145e559551654d5e4b8e6d93b39ad6ef365
Gaurav Phule <gaurav230605@gmail.com>
2025-04-14 21:15:26 +0530

    passed the function to the logout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ export const Navbar = () => {
               <div className='min-w-48 bg-stone-100 rounded flex flex-col shadow-lg gap-4 p-4'>
                 <p onClick={() => navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
                 <p onClick={() => navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
-                <p onClick={() => setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                <p onClick={tokenRemoveFn} className='hover:text-black cursor-pointer'>Logout</p>
               </div>
             </div>
           </div> :
