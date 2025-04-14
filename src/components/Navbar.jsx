48ad6feeb09888bcf36ea0a2d5a7336c4f6c72fb
Gaurav Phule <gaurav230605@gmail.com>
2025-04-14 21:12:57 +0530

    profile to be viewed only if user is logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Cookies from "js-cookie"
 
 export const Navbar = () => {
 
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
-  const [token, setToken] = useState(true);
+  const [token, setToken] = useState(false);
+
+  useEffect(() => {
+  const tokenCheck = Cookies.get('token');
+  if (tokenCheck) {
+    setToken(true);
+  } 
+}, []);
+  
+  const tokenRemoveFn = () =>{
+    const tokenTry = Cookies.get('token');
+    if(tokenTry){
+      Cookies.remove('token', { path: '/' });
+      setToken(false)
+    }
+  }
 
   return (
     <div className='min-w-auto max-w-80% h-16 rounded-2xl shadow-xl my-4 flex flex-row justify-between '>
